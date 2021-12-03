db1361356bc326dd5fabad384a9c4701e3039a8f
monisharajan <monisha.rajan@thoughtworks.com>
2021-12-03 10:25:31 +0530

    Monisha | Add

---
+++ src/test/java/com/bootcamp/cicddemo/CiCdDemoApplicationTests.java
@@ -0,0 +1,15 @@
+package com.bootcamp.cicddemo;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CiCdDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+		Assertions.assertTrue(true);
+	}
+
+}
